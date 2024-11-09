622e51b889848281ddeec6530fc9af487971e58f
Andrei Ciineanu <stefanciineanu@gmail.com>
2024-11-09 23:21:04 +0200

    cmit-1: test git works

---
+++ src/main/java/com/example/storemgmtdemo/StoreMgmtDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.storemgmtdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreMgmtDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreMgmtDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/storemgmtdemo/StoreMgmtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storemgmtdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreMgmtDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
